5076507b94b3cef29432550139d48363a7646284
mvfranzke <michellevonfranzke@gmail.com>
2023-07-20 14:39:23 -0700

    created user.js inside models to hold the user schema and model

---
+++ models/User.js
@@ -0,0 +1,48 @@
+//import mongoose 
+const { Schema, model } = require('mongoose');
+
+//define user schema using the mongoose schema constructor
+//sets the blueprint/structure of userSchema based from the README guideline
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, //regular expression matcing a valid email address
+    },
+    // Array of `_id` values referencing the `Thought` model
+    thoughts: [
+      {
+        type: Schema.Types.ObjectID,
+        ref: "Thought",
+      },
+    ],
+    //Array of `_id` values referencing the `User` model (self-reference)
+    friends: [
+      {
+        type: Schema.Types.ObjectID,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJson: { virtuals: true },
+    id: false,
+  }
+);
+
+//virtual called `friendCount` that retrieves the length of the user's `friends` array field on query
+userSchema.virtual('friendCount').get(function() {return this.friends.length;});
+
+// create the User model using the userSchema
+const User = model('User', userSchema);
+
+// exports User model to be used in other parts of the application
+module.exports = User;
