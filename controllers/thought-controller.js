1b3553dc74fed3e8a0a056734850a4eff656e606
mvfranzke <michellevonfranzke@gmail.com>
2023-07-21 09:33:12 -0700

    create controller folder and embed 2 file thought and user.js to hold CRUD for manipulating user and though data, added pseudo-codes in the meantime for better structure later when coding

---
+++ controllers/thought-controller.js
@@ -0,0 +1,16 @@
+//import models
+
+
+//get all thoughts
+
+// get single thought by ID
+
+// create a thought
+
+// update a thought
+
+//delete a thought
+
+// add reaction to thought
+
+// delete reaction to thought
---
+++ controllers/user-controller.js
@@ -0,0 +1,16 @@
+//import models
+
+//display all users
+
+//get single user by using their id
+
+//create a new user
+
+//update an existing user
+
+//delete a user, cascade to delete user thoughts as well
+
+
+// add other users inside the friends array
+
+// delete other user inside the friends array
