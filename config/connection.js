fb0b02301f5d7b6df33c9b17c379c20ef6ad1a41
mvfranzke <michellevonfranzke@gmail.com>
2023-07-21 09:14:47 -0700

    set up connection bt importing mongoose library, connect to mondodb uri or local uri if not available and export object returned by mongoose.connect()

---
+++ config/connection.js
@@ -0,0 +1,10 @@
+//set up connection bt importing mongoose library, connect to mondodb uri or local uri if not available and export object returned by mongoose.connect()
+
+// import mongoose library
+const mongoose = require ('mongoose');
+
+//connect to mongodb database using the mongo db URI or the default local URI if not available
+mongoose.connect(process.ENV.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialmedia');
+
+//export the connection object returned by mongoose.connect()
+module.exports = mongoose.connection;
