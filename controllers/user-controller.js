19dec39b8a581845f7b9ee4dcdee1e0cdb1372fe
mvfranzke <michellevonfranzke@gmail.com>
2023-07-22 15:54:16 -0700

    set userController object in user-controller to display all user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,16 +1,20 @@
 //import models
+const { User, Thought } = require ('../models');
 
-//display all users
-
-//get single user by using their id
-
-//create a new user
-
-//update an existing user
-
-//delete a user, cascade to delete user thoughts as well
-
-
-// add other users inside the friends array
-
-// delete other user inside the friends array
+const userController = {
+  //display all users
+  async getUSers (req, res) {
+    // Query the database to get all users and exclude the '__v' field from the result and send the fetched user data as a JSON response
+    try {const dbUserdata = await User.find().select('-__v')res.json(dbUserdata);}
+    //error handling
+    catch(err) {
+      console.log(err);res.status(500).json(err);
+    }
+  }
+  //get single user by using their id
+  //create a new user
+  //update an existing user
+  //delete a user, cascade to delete user thoughts as well
+  // add other users inside the friends array
+  // delete other user inside the friends array
+};
