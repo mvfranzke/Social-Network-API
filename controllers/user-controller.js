6721cb6d378622045bf76abc115efe4af01a8362
mvfranzke <michellevonfranzke@gmail.com>
2023-07-22 17:20:42 -0700

    create route and route handler to locate user using userid

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,18 +3,38 @@ const { User, Thought } = require ('../models');
 
 const userController = {
   //display all users
-  async getUsers (req, res) {
+  async getUsers(req, res) {
     // Query the database to get all users and exclude the '__v' field from the result and send the fetched user data as a JSON response
-    try {const dbUserdata = await User.find().select('-__v');
-    res.json(dbUserdata);}
-    //error handling
-    catch(err) {
-      console.log(err);res.status(500).json(err);
+    try {
+      const dbUserdata = await User.find().select("-__v");
+      res.json(dbUserdata);
+    } catch (err) {
+      //error handling
+      console.log(err);
+      res.status(500).json(err);
     }
   },
   //get single user by using their id
+  async getUserById(req, res) {
+    try {
+      const dbUserData = await User.findOne({ _id: req.params.userId })
+        .select('-__v')
+        .populate('friends')
+        .populate('thoughts');
+
+      if (!dbUserData) {
+        return res.status(404).json({ message: "No user with this id!" });
+      }
+
+      res.json(dbUserData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
   //create a new user
-    async createUser(req, res) {
+  async createUser(req, res) {
     try {
       const dbUserData = await User.create(req.body);
       res.json(dbUserData);
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,8 +2,11 @@ const router = require('express').Router();
 const {
   getUsers,
   createUser,
+  getUserById,
 } = require ('../../controllers/user-controller');
 
 router.route('/').get(getUsers).post(createUser);
+
+router.route('/:userId').get(getUserById);
 
 module.exports = router;
