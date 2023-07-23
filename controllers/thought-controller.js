0406fda814c67cdb72d29a6b0505c174cc035d41
mvfranzke <michellevonfranzke@gmail.com>
2023-07-22 23:10:57 -0700

    added function in thought-controller to add thoughts inside the array and return it as Objectid, set route in thought-routs

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,16 +1,40 @@
 //import models
+const { Thought, User } = require('../models')
 
+const thoughtController = {
+  // create a thought
+  async createThought(req, res) {
+    try {
+      const dbThoughtData = await Thought.create(req.body);
 
-//get all thoughts
+      const dbUserData = await User.findOneAndUpdate(
+        { _id: req.body.userId },
+        { $push: { thoughts: dbThoughtData._id } },
+        { new: true }
+      );
 
-// get single thought by ID
+      if (!dbUserData) {
+        return res
+          .status(404)
+          .json('Thought saved but no user found');
+      }
 
-// create a thought
+      res.json({
+        success: true,
+        message: "Thought successfully created",
+        user: dbUserData,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  //get all thoughts
+  // get single thought by ID
+  // update a thought
+  //delete a thought
+  // add reaction to thought
+  // delete reaction to thought
+};
 
-// update a thought
-
-//delete a thought
-
-// add reaction to thought
-
-// delete reaction to thought
+module.exports = thoughtController;
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,4 +1,10 @@
 const router = require("express").Router();
 
+const { 
+  createThought } = 
+  require("../../controllers/thought-controller");
+
+router.route('/').post(createThought);
+
 
 module.exports = router;
